d6905e11fd93f460fbe2f9798315d48e7a83f0b3
Taro <15029352778@163.com>
2018-05-04 23:54:46 +0800

    feat: add console for function declarations and call
    You can print what the parameters of the function pass when the function is declared, and what the
    print method returns when the function is called.
    
    BREAKING CHANGE: add function logger

--- example/source.vue
+++ example/source.vue
@@ -7,7 +7,50 @@
 <script>
     export default {
         mounted () {
+            /**
+             * test assignment
+             */
             let a = 123 // {#}
+
+            let b
+            b = 456 // {#}
+
+            /**
+             * test function declaration
+             */
+            this.testFunc1("This is testFunc1")
+            this.testFunc2("This is testFunc2", {
+                name: 'Yangxin',
+                age: '15'
+            })
+
+            /**
+             * test function call
+             */
+            this.testFunc3() // {#} 
+            
+            // no support for the time being
+            // this.testFunc4(1, { // {#}
+            //     count: 1
+            // }) 
+            // recommend to use this like this
+            this.testFunc4(1, {count: 1}) // {#}
+        },
+
+        methods: {
+            testFunc1 (testStr) { // {#} 
+                // console.log(testStr)
+            },
+            testFunc2 (testSrt1, testObject2) { // {#}
+
+            },
+
+            testFunc3 () {
+                return 'Hello, I\'m return!'
+            },
+            testFunc4 (num, data) {
+                return num + data.count
+            }
         }
     }
 </script>
