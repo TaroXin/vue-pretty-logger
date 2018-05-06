e6b336c55adc4c94016456d8fa3904a9ebb6ece5
Taro <15029352778@163.com>
2018-05-06 16:19:30 +0800

    test: add unit test for logger parser

--- test/loader-parser.test.js
+++ test/loader-parser.test.js
@@ -2,6 +2,18 @@ const loaderParser = require('../lib/utils/logger-parser')
 
 test('test parse logger with normal', () => {
     let test = 'let a = 0 // {#}'
-    let testResult = 'let a = 0\nconsole.log(a)'
+    let testResult = 'let a = 0\nconsole.log(a)\n'
+    expect(loaderParser.parseLogger(test, {hook: '#'})).toBe(testResult)
+})
+
+test('test function declaration', () => {
+    let test = 'test () { // {#}'
+    let testResult = 'test () {'
+    expect(loaderParser.parseLogger(test, {hook: '#'})).toBe(testResult)
+})
+
+test('test function declaration with params', () => {
+    let test = 'test (a, b) { // {#}'
+    let testResult = 'test (a, b) {\nconsole.log(a, b)\n'
     expect(loaderParser.parseLogger(test, {hook: '#'})).toBe(testResult)
 })
