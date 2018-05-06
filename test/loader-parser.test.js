50173f0fa902060750a1f68cbdd7d05b5b488d18
Taro <15029352778@163.com>
2018-05-06 14:49:01 +0800

    test: modify unit test for logger parser

--- test/loader-parser.test.js
+++ test/loader-parser.test.js
@@ -3,5 +3,5 @@ const loaderParser = require('../lib/utils/logger-parser')
 test('test parse logger with normal', () => {
     let test = 'let a = 0 // {#}'
     let testResult = 'let a = 0\nconsole.log(a)'
-    expect(loaderParser.parseLogger(test)).toBe(testResult)
+    expect(loaderParser.parseLogger(test, {hook: '#'})).toBe(testResult)
 })
